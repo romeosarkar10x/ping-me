f9b95f3237f01bc1da232e722c3b99b0d63165ec
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-05-21 21:57:18 +0530

    Removed baseURL i.e. default: '/' for cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,7 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 
-function getBaseURL(): string {
-    const githubRepository = process.env.GITHUB_REPOSITORY;
-
-    if (githubRepository === undefined) {
-        return "/";
-    }
-
-    const [, repo] = githubRepository.split("/");
-    return `/${repo}`;
-}
-
 export default defineConfig({
-    base: getBaseURL(),
     plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 
     resolve: {
